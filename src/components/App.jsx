0c352ee6b4feffc21bf2b6340d2c51d0caa60574
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-06-07 15:13:48 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header'
+import Footer from './footer';
+import Note from './Note';
+
+
+
+function App (){
+    return <div> 
+<Header></Header>
+<Note></Note>
+<Footer></Footer>
+    </div>
+}
+
+
+
+export default App;
