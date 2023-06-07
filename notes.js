9f413b845bb33ba4a275ed5e95b54dbb6be528af
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-06-07 16:24:22 +0530

    Initial Commit

---
+++ notes.js
@@ -0,0 +1,20 @@
+const notes = [
+    {
+      key: 1,
+      title: "day 1",
+      content: "Programming ,coding"
+    },
+    {
+      key: 2,
+      title: "day 2",
+      content: "Appointment for doctor"
+    },
+    {
+      key: 3,
+      title: "day 3",
+      content: "Badminton classes"
+    }
+  ];
+  
+  export default notes;
+  
